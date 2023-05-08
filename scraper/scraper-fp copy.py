f0ff18b59b29dd24da623f65b9ed6e3fd1ad7452
SDW <stephenoilesanmi@gmail.com>
2023-05-09 00:50:16 +0100

    added webpage_loader and popup_handler functions

--- scraper/scraper-fp copy.py
+++ scraper/scraper-fp copy.py
@@ -10,6 +10,7 @@ from functools import partial
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
@@ -65,8 +66,7 @@ def create_console_handler(colored=True, level=logging.DEBUG, detailed_logs=Fals
     return console_handler
 
 
-def log_event(logger, message, **kwargs):
-    level = kwargs.get('level', logging.INFO)
+def log_event(logger, level, message):
     logger.log(level, message)
 
 
@@ -102,10 +102,76 @@ def create_config(aws_access_key: str, aws_secret_key: str, aws_region_name: str
 
 
 
+# ================================================ WEBPAGE LOADER ================================================
+
+def create_webdriver(options=None, service=None) -> webdriver.Chrome:
+    if options is None:
+        options = webdriver.ChromeOptions()
+    if service is None:
+        service = Service(executable_path=ChromeDriverManager().install())
+    
+    return webdriver.Chrome(options=options, service=service)
+    
+
+def load_webpage(chrome_driver: webdriver.Chrome, url: str, logger: logging.Logger) -> webdriver.Chrome:
+    log_event(logger, logging.DEBUG, ">>> Loading webpage using Selenium ...")
+    chrome_driver.get(url)
+    sleep(3)
+    return chrome_driver
+
+
+def check_page_title(chrome_driver: webdriver.Chrome, expected_title: str, logger: logging.Logger) -> None:
+    assert expected_title in chrome_driver.title, f"ERROR: Unable to load site for {expected_title} ... "
+    log_event(logger, logging.DEBUG, ">>> Webpage successfully loaded ...")
+
+
+def load_prem_league_table_webpage(url: str, logger: logging.Logger, options=None, service=None) -> webdriver.Chrome:
+    chrome_driver = create_webdriver(options, service)
+    chrome_driver = load_webpage(chrome_driver, url, logger)
+    check_page_title(chrome_driver, "Premier League", logger)
+    
+    return chrome_driver
+
+
+
+
+# ================================================ POPUP HANDLER ================================================
+
+def find_popup_element(chrome_driver: webdriver.Chrome, timeout: int) -> WebElement:
+    wait = WebDriverWait(chrome_driver, timeout)
+
+    return wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[8]/div[2]/div[1]/div[1]/button/i')))
+
+
+def close_popup(close_popup_box: WebElement, logger: logging.Logger) -> None:
+    close_popup_box.click()
+    log_event(logger, logging.DEBUG, f'>>>>   Closing cookie pop-up window ...')
+
+
+def close_popup_box_for_prem_league_table_webpage(chrome_driver: webdriver.Chrome, logger: logging.Logger, timeout: int = 5) -> None:
+    try:
+        close_popup_box = find_popup_element(chrome_driver, timeout)
+        close_popup(close_popup_box, logger)
+    except Exception as e:
+        log_event(logger, logging.ERROR, f'No cookie pop-up window to close...let\'s begin scraping for league standings !!') 
+
+
+
+
+
+
+
+
+
+
+
 
 # Initialize the functions 
 
 def main():
+
+    match_date                      =   '2023-May-09'
+    football_url                    =   f'https://www.twtd.co.uk/league-tables/competition:premier-league/daterange/fromdate:2022-Jul-01/todate:{match_date}/type:home-and-away/'
 
     # ================================================ LOGGER ================================================
     logger_name         =   __name__
@@ -114,7 +180,7 @@ def main():
 
     logger              =   create_logger(logger_name, log_level)
     file_handler        =   create_file_handler(local_filepath, log_level)
-    console_handler     =   create_console_handler(colored=False, level=log_level, detailed_logs=False)
+    console_handler     =   create_console_handler(colored=False, level=log_level, detailed_logs=False) 
 
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
@@ -125,17 +191,16 @@ def main():
     log_event_as_critical   =   partial(log_event, logger, level=logging.CRITICAL)
     log_event_as_error      =   partial(log_event, logger, level=logging.ERROR)
 
-    log_event_as_debug("This is debug message")
-    log_event_as_info("This is info message")
-    log_event_as_warning("This is warning message")
-    log_event_as_critical("This is critical message")
-    log_event_as_error("This is error message")
+    # log_event_as_debug("This is debug message")
+    # log_event_as_info("This is info message")
+    # log_event_as_warning("This is warning message")
+    # log_event_as_critical("This is critical message")
+    # log_event_as_error("This is error message")
 
 
 
 # ================================================ CONFIG ================================================
 
-    
     aws_access_key          =   os.getenv("AWS_ACCESS_KEY")
     aws_secret_key          =   os.getenv("AWS_SECRET_KEY")
     aws_region_name         =   os.getenv("S3_REGION") 
@@ -148,7 +213,15 @@ def main():
 
 
 
+# ================================================ WEBPAGE LOADER ================================================
 
+    chrome_driver = load_prem_league_table_webpage(football_url, logger)
+    close_popup_box_for_prem_league_table_webpage(chrome_driver, logger)
+
+
+
+
+    chrome_driver.quit()
 
 
 
